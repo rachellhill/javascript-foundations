230e407b1ba22b22d5e13095d6662baad27113db
Rachel Hill <hillrachel187@gmail.com>
2022-02-07 15:09:06 -0700

    Complete unicorn file

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,26 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    if (color === undefined) {
+      this.color = 'white';
+    } else {
+      this.color = color;
+      }
+    }
+    isWhite() {
+      if (this.color !== 'white') {
+        return false;
+      }
+    }
+
+    says(message) {
+      return `**;* ${message} *;**`;
+    }
   }
-}
+
 
 module.exports = Unicorn;
+// 1. read the test
+// 2. ask yourself if its going to fail and why
+// 3. run the test
+// 4. make it pass
--- mythical-creatures/test/unicorn-test.js
+++ mythical-creatures/test/unicorn-test.js
@@ -15,37 +15,33 @@ describe('Unicorn', function() {
 
   it('should have a name', function() {
     var unicorn = new Unicorn('Bree');
-
     assert.equal(unicorn.name, 'Bree');
+    // assert is our assertion library
+    // assert.equal(actual, expected)
   });
 
   it('should be able to have a different name', function() {
     var unicorn = new Unicorn('Pam');
-
     assert.equal(unicorn.name, 'Pam');
   });
 
   it('should have a color', function() {
     var unicorn = new Unicorn('TayTay', 'blue');
-    
     assert.equal(unicorn.color, 'blue');
   });
 
-  it.skip('should be white as default', function() {
+  it('should be white as default', function() {
     var unicorn = new Unicorn('Louisa');
-
     assert.equal(unicorn.color, 'white');
   });
 
-  it.skip('should refute isWhite', function() {
+  it('should refute isWhite', function() {
     var unicorn = new Unicorn('Steve', 'red');
-
     assert.equal(unicorn.isWhite(), false);
   });
 
-  it.skip('should say sparklying stuff', function() {
+  it('should say sparklying stuff', function() {
     var unicorn = new Unicorn('Brenna');
-    
     assert.equal(unicorn.says('Wonderful!'), '**;* Wonderful! *;**');
     assert.equal(unicorn.says('Batman has a great smile'), '**;* Batman has a great smile *;**');
   });
