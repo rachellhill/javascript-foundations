61b346e34827945d573ed49e52ef37846f4fd90e
Rachel Hill <hillrachel187@gmail.com>
2022-02-09 09:58:22 -0700

    Complete hobbit test

---
+++ mythical-creatures/exercises/hobbit.js
@@ -0,0 +1,27 @@
+class Hobbit {
+  constructor(nameObject) {
+    this.name = nameObject.name;
+    this.age = 0;
+    this.adult = false;
+    this.old = false;
+    this.hasRing = false;
+  }
+  celebrateBirthday() {
+    this.age++;
+    if (this.age > 32) {
+      this.adult = true;
+    } if (this.age > 100) {
+      this.old = true;
+    }
+  }
+  getRing() {
+    if (this.name === 'Frodo') {
+      this.hasRing = true;
+      return 'Here is the ring!';
+    } return 'You can\'t have it!';
+  }
+}
+
+
+
+module.exports = Hobbit;
