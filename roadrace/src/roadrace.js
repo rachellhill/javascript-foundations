d41c71f3fd344530f1da5d4b4b41978342af1765
Rachel Hill <hillrachel187@gmail.com>
2022-02-15 19:27:49 -0700

    Complete runner, roadrace and magician

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,26 @@
 class Roadrace {
+  constructor(race) {
+    this.name = race.title; // Rock N Roll Half Marathon'
+    this.location = race.city; // "Las Vegas"
+    this.distance = 0; // 13.1
+    this.participants = []; // mike
+  }
 
+  setDistance(distance) {
+    this.distance += distance; // 13.1
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+  }
+
+  registerParticipants(runner) { // mike
+    this.participants.push(runner);
+  }
+
+  completeRace() {
+    for (var i = 0; i < this.participants.length; i++) {
+      this.participants[i].completedRaces.push(this.name);
+      this.participants[i].runSomeMiles(this.distance);
+    }
+  }
 }
 
 module.exports = Roadrace;
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,25 @@
 class Runner {
+  constructor(name, age) {
+    this.name = name; // Mike
+    this.age = age; // 36
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = []; //'Rock N Roll Half Marathon'
+  }
 
+  runSomeMiles(miles) {
+    this.milesRun += miles;
+    this.fitness += miles;
+  }
+
+  stretch() {
+    this.fitness += .5;
+  }
+
+  runRace(race, distance) {
+    this.completedRaces.unshift(race);
+    this.runSomeMiles(distance);
+  }
 }
 
 module.exports = Runner;
